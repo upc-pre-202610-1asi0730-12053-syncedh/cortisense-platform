d4c4057643d42af2138f84b3e17d96b9eafbd4e9
Marce <marsredflag@gmail.com>
2026-06-14 10:17:58 -0500

    fix(shared): fix ieventhandler dependencies.

--- SyncedHealth.Center.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++ SyncedHealth.Center.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -1,7 +1,9 @@
-using SyncedHealth.Center.Platform.Shared.Domain
+using SyncedHealth.Center.Platform.Shared.Domain.Model.Events;
 
+using Cortex.Mediator.Notifications;
 
 namespace SyncedHealth.Center.Platform.Shared.Application.Internal.EventHandlers;
+
 
 public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
 {
