9a8d7d32a8a7de9e0aa6b8f0cb3ea8afff9da614
Marce <marsredflag@gmail.com>
2026-06-14 12:22:44 -0500

    add app db context class.

---
+++ SyncedHealth.Center.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/AppDbContext.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Interceptors;
+
+namespace SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+
+/// <summary>
+///     Application database context for the Learning Center Platform
+/// </summary>
+/// <param name="options">
+///     The options for the database context
+/// </param>
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    /// <inheritdoc />
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        // Apply audit timestamp interceptor for all IAuditableEntity implementations
+        builder.AddInterceptors(new AuditableEntityInterceptor());
+        base.OnConfiguring(builder);
+    }
+
+
+    /// <summary>
+    ///     On creating the database model
+    /// </summary>
+    /// <remarks>
+    ///     This method is used to create the database model for the application.
+    /// </remarks>
+    /// <param name="builder">
+    ///     The model builder for the database context
+    /// </param>
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+
+        // General Naming Convention for the database objects
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
