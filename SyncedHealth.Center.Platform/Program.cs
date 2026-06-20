bb90398c0ed52c4715e4a9cab05e604ea3231e26
Ghiou Justinn Mauricio Silva <gjustinnmauricios001@gmail.com>
2026-06-19 23:46:17 -0500

    feat: register audit compliance services

--- SyncedHealth.Center.Platform/Program.cs
+++ SyncedHealth.Center.Platform/Program.cs
@@ -1,5 +1,11 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using SyncedHealth.Center.Platform.AuditCompliance.Application.CommandServices;
+using SyncedHealth.Center.Platform.AuditCompliance.Application.Internal.CommandServices;
+using SyncedHealth.Center.Platform.AuditCompliance.Application.Internal.QueryServices;
+using SyncedHealth.Center.Platform.AuditCompliance.Application.QueryServices;
+using SyncedHealth.Center.Platform.AuditCompliance.Domain.Repositories;
+using SyncedHealth.Center.Platform.AuditCompliance.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
 using SyncedHealth.Center.Platform.Shared.Domain.Repositories;
 using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
@@ -13,9 +19,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+c.IncludeXmlComments(xmlPath);
 });
 
 // Add Database Connection
@@ -23,34 +29,39 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 if (connectionString == null)
 {
-    throw new InvalidOperationException("Connection string not found.");
+throw new InvalidOperationException("Connection string not found.");
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    if (builder.Configuration.GetValue<bool>("UseInMemoryDatabase"))
-    {
-        options.UseInMemoryDatabase("CortisenseDb");
-    }
-    else
-    {
-        options.UseMySQL(connectionString)
-            .LogTo(Console.WriteLine, LogLevel.Information)
-            .EnableSensitiveDataLogging()
-            .EnableDetailedErrors();
-    }
+if (builder.Configuration.GetValue<bool>("UseInMemoryDatabase"))
+{
+options.UseInMemoryDatabase("CortisenseDb");
+}
+else
+{
+options.UseMySQL(connectionString)
+.LogTo(Console.WriteLine, LogLevel.Information)
+.EnableSensitiveDataLogging()
+.EnableDetailedErrors();
+}
 });
 
 // Shared Bounded Context Injection Configuration
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Audit Compliance Bounded Context Injection Configuration
+builder.Services.AddScoped<IAuditLogRepository, AuditLogRepository>();
+builder.Services.AddScoped<IAuditLogCommandService, AuditLogCommandService>();
+builder.Services.AddScoped<IAuditLogQueryService, AuditLogQueryService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
