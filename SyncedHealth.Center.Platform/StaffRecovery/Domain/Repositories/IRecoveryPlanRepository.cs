4121c77ff1f6205da9d555f4d92fcbe880603c3c
unknown <jsantiagoatanacio@gmail.com>
2026-06-20 18:37:41 -0500

    feat(staff-recovery): define custom repository contract

--- SyncedHealth.Center.Platform/StaffRecovery/Domain/Repositories/IRecoveryPlanRepository.cs
+++ SyncedHealth.Center.Platform/StaffRecovery/Domain/Repositories/IRecoveryPlanRepository.cs
@@ -3,7 +3,9 @@ using SyncedHealth.Center.Platform.StaffRecovery.Domain.Model.Aggregates;
 
 namespace SyncedHealth.Center.Platform.StaffRecovery.Domain.Repositories;
 
-public interface IRecoveryPlanRepository : IBaseRepository<RecoveryPlan>
+public interface IRecoveryPlanRepository : IBaseRepository<RecoveryPlan> // O la clase base que uses
 {
     Task<IEnumerable<RecoveryPlan>> FindByMedicalStaffIdAsync(int medicalStaffId);
+    Task<IEnumerable<RecoveryPlan>> FindByStatusAsync(string status);
+    Task<IEnumerable<RecoveryPlan>> FindBySuggestedRestDaysAsync(int suggestedRestDays);
 }
