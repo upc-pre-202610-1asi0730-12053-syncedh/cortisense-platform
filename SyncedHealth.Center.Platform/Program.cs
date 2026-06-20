20cf3d3bee5b4ca8e8e4e022c4a42222170dd020
Marce <marsredflag@gmail.com>
2026-06-19 19:36:01 -0500

    feat(iam): add user command service.

--- SyncedHealth.Center.Platform/Program.cs
+++ SyncedHealth.Center.Platform/Program.cs
@@ -1,5 +1,16 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using SyncedHealth.Center.Platform.Iam.Application.Acl;
+using SyncedHealth.Center.Platform.Iam.Application.CommandServices;
+using SyncedHealth.Center.Platform.Iam.Application.Internal.CommandServices;
+using SyncedHealth.Center.Platform.Iam.Application.Internal.OutboundServices;
+using SyncedHealth.Center.Platform.Iam.Application.Internal.QueryServices;
+using SyncedHealth.Center.Platform.Iam.Application.QueryServices;
+using SyncedHealth.Center.Platform.Iam.Domain.Repositories;
+using SyncedHealth.Center.Platform.Iam.Infrastructure.Hashing.BCrypt.Services;
+using SyncedHealth.Center.Platform.Iam.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using SyncedHealth.Center.Platform.Iam.Infrastructure.Tokens.Jwt.Services;
+using SyncedHealth.Center.Platform.Iam.Interfaces.Acl;
 using SyncedHealth.Center.Platform.Shared.Domain.Repositories;
 using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using SyncedHealth.Center.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
@@ -45,6 +56,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
+// IAM Bounded Context Injection Configuration
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserCommandService, UserCommandService>();
+builder.Services.AddScoped<IUserQueryService, UserQueryService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IHashingService, HashingService>();
+builder.Services.AddScoped<IIamContextFacade, IamContextFacade>();
 
 var app = builder.Build();
 
