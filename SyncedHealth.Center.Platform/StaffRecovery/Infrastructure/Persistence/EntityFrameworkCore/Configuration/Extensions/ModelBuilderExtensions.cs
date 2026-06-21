1fc9c39eb8f74622cc971946e35d64ad43e1356d
Ghiou Justinn Mauricio Silva <gjustinnmauricios001@gmail.com>
2026-06-20 23:04:14 -0500

    refactor(staff-recovery): align recovery plan ef mapping

--- SyncedHealth.Center.Platform/StaffRecovery/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ SyncedHealth.Center.Platform/StaffRecovery/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -5,21 +5,55 @@ namespace SyncedHealth.Center.Platform.StaffRecovery.Infrastructure.Persistence.
 
 public static class ModelBuilderExtensions
 {
-    public static ModelBuilder ConfigureStaffRecoveryContext(this ModelBuilder builder)
+    public static void ApplyStaffRecoveryConfiguration(this ModelBuilder builder)
     {
-
         builder.Entity<RecoveryPlan>().ToTable("recovery_plans");
 
+        builder.Entity<RecoveryPlan>().HasKey(recoveryPlan => recoveryPlan.Id);
 
-        builder.Entity<RecoveryPlan>().HasKey(rp => rp.Id);
-        builder.Entity<RecoveryPlan>().Property(rp => rp.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.Id)
+            .HasColumnName("id")
+            .IsRequired()
+            .ValueGeneratedOnAdd();
 
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.MedicalStaffId)
+            .HasColumnName("medical_staff_id")
+            .IsRequired();
 
-        builder.Entity<RecoveryPlan>().Property(rp => rp.MedicalStaffId).IsRequired();
-        builder.Entity<RecoveryPlan>().Property(rp => rp.Description).IsRequired().HasMaxLength(500);
-        builder.Entity<RecoveryPlan>().Property(rp => rp.SuggestedRestDays).IsRequired();
-        builder.Entity<RecoveryPlan>().Property(rp => rp.Status).IsRequired().HasMaxLength(50);
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.Description)
+            .HasColumnName("description")
+            .HasMaxLength(500)
+            .IsRequired();
 
-        return builder;
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.SuggestedRestDays)
+            .HasColumnName("suggested_rest_days")
+            .IsRequired();
+
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.Status)
+            .HasColumnName("status")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.CreatedAt)
+            .HasColumnName("created_at");
+
+        builder.Entity<RecoveryPlan>()
+            .Property(recoveryPlan => recoveryPlan.UpdatedAt)
+            .HasColumnName("updated_at");
+
+        builder.Entity<RecoveryPlan>()
+            .HasIndex(recoveryPlan => recoveryPlan.MedicalStaffId);
+
+        builder.Entity<RecoveryPlan>()
+            .HasIndex(recoveryPlan => recoveryPlan.Status);
+
+        builder.Entity<RecoveryPlan>()
+            .HasIndex(recoveryPlan => recoveryPlan.SuggestedRestDays);
     }
 }
