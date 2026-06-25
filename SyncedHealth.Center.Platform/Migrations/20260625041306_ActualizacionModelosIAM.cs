fee028e1f88b9f123bda22a809cc9bb215676085
Marce <marsredflag@gmail.com>
2026-06-25 00:30:48 -0500

    test 1

---
+++ SyncedHealth.Center.Platform/Migrations/20260625041306_ActualizacionModelosIAM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SyncedHealth.Center.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionModelosIAM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // COMPLETAMENTE VACÍO: Los cambios ya existen en Azure gracias a la migración anterior
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // COMPLETAMENTE VACÍO
+        }
+    }
+}
