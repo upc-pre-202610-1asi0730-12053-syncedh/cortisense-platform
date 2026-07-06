7ab0e72ac377986fd61103f7b87c560a8f9bb94c
Marce <marsredflag@gmail.com>
2026-07-05 19:32:02 -0500

    more additions

---
+++ SyncedHealth.Center.Platform/Migrations/20260705204619_UpdateModels.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncedHealth.Center.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 5, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(2520), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 102,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 5, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(3033), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 203,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 5, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(3034), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 204,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 5, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(3034), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 101,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 7, 4, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(7989), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 6, 20, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(7889), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 102,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 8, 0, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(8498), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 7, 16, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(8496), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 103,
+                columns: new[] { "check_in_at", "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 5, 10, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(8500), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 22, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(8499), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 10, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(8499), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 204,
+                columns: new[] { "check_in_at", "check_out_at", "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 4, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(1853), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 4, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(1853), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 4, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(1852), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 4, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(1849), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 205,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 8, 4, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(2332), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 7, 20, 46, 18, 622, DateTimeKind.Unspecified).AddTicks(2331), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "subscriptions",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "started_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 5, 20, 46, 18, 621, DateTimeKind.Unspecified).AddTicks(7210), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 4, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(4506), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 102,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 4, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(5469), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 203,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 4, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(5471), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "risk_assessments",
+                keyColumn: "id",
+                keyValue: 204,
+                column: "last_updated_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 4, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(5472), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 101,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 6, 6, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(509), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(258), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 102,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 7, 2, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(1889), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 6, 18, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(1885), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 103,
+                columns: new[] { "check_in_at", "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 4, 12, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(1894), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 5, 0, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(1893), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 4, 12, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(1892), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 204,
+                columns: new[] { "check_in_at", "check_out_at", "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 3, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(3171), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 4, 6, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(3172), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 4, 6, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(3170), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 3, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(3167), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "shift_records",
+                keyColumn: "id",
+                keyValue: 205,
+                columns: new[] { "scheduled_end", "scheduled_start" },
+                values: new object[] { new DateTimeOffset(new DateTime(2026, 7, 7, 6, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(4303), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2026, 7, 6, 22, 53, 53, 168, DateTimeKind.Unspecified).AddTicks(4301), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "subscriptions",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "started_at",
+                value: new DateTimeOffset(new DateTime(2026, 7, 4, 22, 53, 53, 167, DateTimeKind.Unspecified).AddTicks(8565), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
